fe3eb4808227341adca1303aceace4a49f6b6dbe
Peter <peev.petar@gmail.com>
2017-10-17 15:46:20 +0300

    working Calculator page  and controller

--- HTTPMiniServer/ByTheCakeApplication/Controllers/CalculatorController.cs
+++ HTTPMiniServer/ByTheCakeApplication/Controllers/CalculatorController.cs
@@ -1,6 +1,6 @@
-﻿
-namespace HTTPMiniServer.ByTheCakeApplication.Controllers
+﻿namespace HTTPMiniServer.ByTheCakeApplication.Controllers
 {
+   using System;
    using System.Collections.Generic;
    using Infrastructure;
    using Server.HTTP.Contracts;
@@ -14,9 +14,45 @@ namespace HTTPMiniServer.ByTheCakeApplication.Controllers
          ["showResults"] = "none"
       });
 
-      public IHttpResponse ParseInputFromFormData(string firstDigit,string operand , string secondDigit)
+      public IHttpResponse ParseInputFromFormData(string firstDigit, string operand, string secondDigit)
       {
-         return null;
+         var multiplication = "*";
+         var dividion = "/";
+         var sum = "+";
+         var subtraction = "-";
+
+         var result = 0.0m;
+         var parsedFirstDigit = decimal.Parse(firstDigit);
+         var parsedSecondDigit = decimal.Parse(secondDigit);
+
+
+         if (operand != dividion && operand != multiplication && operand != sum && operand != subtraction)
+         {
+
+            throw new InvalidOperationException();
+
+         }
+         switch (operand)
+         {
+            case "*":
+               result = parsedFirstDigit * parsedSecondDigit;
+               break;
+            case "/":
+               result = parsedFirstDigit / parsedSecondDigit;
+               break;
+            case "+":
+               result = parsedFirstDigit + parsedSecondDigit;
+               break;
+            case "-":
+               result = parsedFirstDigit - parsedSecondDigit;
+               break;
+         }
+
+         return this.FileViewResponse(@"calculator\calculator", new Dictionary<string, string>
+         {
+            ["result"] = result.ToString()
+         });
+
 
       }
    }
